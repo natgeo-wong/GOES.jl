28b57d1dbab97763745f774be8b70d3279fa71fe
Nathanael Wong <natgeo.wong@outlook.com>
2026-05-06 13:47:21 -0400

    Updating downloading of GOES lon/lat files

--- src/lonlat.jl
+++ src/lonlat.jl
@@ -15,7 +15,7 @@ function goeslonlat(gds :: GOESDataset)
     end
     fzip = joinpath(gds.mask,"goes_$(position)-$(sector).zip")
     fnc  = joinpath(gds.mask,"goes_$(position)-$(sector).nc")
-    if !isfile(fzip); downloadgoeslonlat(gds,fzip,fnc) end
+    if !isfile(fnc); downloadgoeslonlat(gds,fzip,fnc) end
     
     ds = NCDataset(fnc)
     lon = nomissing(ds["longitude"][:,:],NaN)
@@ -32,6 +32,8 @@ function downloadgoeslonlat(
     fnc  :: AbstractString
 )
 
+    @info "$(modulelog()) - Downloading the relevant longitude and latitude grid for GOES Satellite $(gds.satellite) and Sector $(gds.sector)"
+
     if gds.satellite == 16 || gds.satellite == 19
         position = 19
     elseif gds.satellite == 17 || gds.satellite == 18
@@ -46,9 +48,10 @@ function downloadgoeslonlat(
 
     download("https://www.star.nesdis.noaa.gov/atmospheric-composition-training/documents/goes$(position)_abi_$(sector)_lat_lon.zip", fzip)
 
-    run(`unzip $fzip`); rm(fzip,force=true)
+    fdir = dirname(fnc)
+    run(`unzip $fzip -d $fdir`); rm(fzip,force=true)
 
-    mv(joinpath(gds.mask,"goes$(position)_abi_$(sector)_lat_lon.nc"), fnc)
+    mv(joinpath(fdir,"goes$(position)_abi_$(sector)_lat_lon.nc"), fnc)
 
     return nothing
 
